e62026b26d6d61ae0563e0434cfca67e6864d2c7
Timothy Dowling <timothy.dowling@me.com>
2018-12-05 22:03:47 -0500

    Setup initial main file.  Added MakeFile

---
+++ CC1/main.cpp
@@ -0,0 +1,13 @@
+/* Coding Challenge #1
+ * Starfield
+ * Created By Tim Dowling
+ * Date: Dec 5, 2018
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Starfield Completed" << std::endl;
+    return 0;
+}
