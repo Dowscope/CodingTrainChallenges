ac0f0772c5e46c7bc46703e181398c1e9c7dcaac
Timothy Dowling <timothy.dowling@me.com>
2018-12-15 19:57:23 -0500

    Started a unity version.

---
+++ CC1/Unity/StarField/Assets/Scripts/Star.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star {
+
+	public int X { get; protected set; }
+	public int Y { get; protected set; }
+
+	public Star() {
+		Debug.Log("The Screen Width is " + Screen.Width);
+	}
+}
